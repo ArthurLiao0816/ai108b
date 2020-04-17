304ed26e6e377f8857bdffc562c23aa7f036326e
ArthurLiao0816 <ArthurLiao0816@yahoo.com>
2020-04-17 14:56:57 +0800

    .\ANDg.py

--- homework/AND Gate/ANDg.py
+++ homework/AND Gate/ANDg.py
@@ -1,20 +1,34 @@
-import gd2 as g
-import math as m
+import gd2
 import numpy as np
 
 def sig(t):
-    return 1.0/(1.0+m.exp(-t))
-
-A = np.array([[0.0, 0, 0],
-                [0, 1, 0],
-                [1, 0, 0],
-                [1, 1, 1]])
-B = np.array([[0.0, 0, 0, 1]]).transpose()
+    return 1/(1+np.exp(-t))
 
 def f(p):
     X = p
-    Y = A.dot(X)    #AX
-    return norm(Y-B)
+    Y = np.dot(A, X)
+    for i in Y:
+        i = sig(i)
+        i = 1 if i >= 0.5 else 0
+    return (np.linalg.norm(Y - B, 2))**2
+A = np.array([[0, 0, 1],  # A矩陣為輸入
+            [0, 1, 1],
+            [1, 0, 1],
+            [1, 1, 1]])
+                
+B = np.array([[0.0, 0, 0, 1]])  # B矩陣為標準答案
 
-p = np.array([0.0, 0 ,0])
-g.gradientDescendent(f, p)
+p = np.array([0.0, 0 ,0])   # 設定 w1、w2、b 的初始值
+
+p = gd2.gradientDescendent(f, p)  # 用梯度下降法找最低點
+
+print("\n\nw1 = {}, w2 = {}, b = {}" .format(p[0], p[1], p[2]))
+
+C = np.dot(A, p)
+result = []
+for i in C:
+    result.append(1) if i >= 0.5 else result.append(0)
+
+print("\tx y | o")
+for i in range(len(result)):
+    print("\t{} {} | {}".format(A[i][0], A[i][1], result[i]))
