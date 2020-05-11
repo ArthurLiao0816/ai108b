38be2e2aaebd158d90d63d10ddfb6749bbec4265
ArthurLiao0816 <ArthurLiao0816@yahoo.com>
2020-05-11 18:38:14 +0800

    .\fftfreq.py

--- homework/fftfreq/fftfreq.py
+++ homework/fftfreq/fftfreq.py
@@ -2,10 +2,8 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 np.set_printoptions(precision=4, suppress=True)
-freq = random.randint(0,5) #設定頻率
-sR = 10     #取樣頻率
-sI = 1/sR   #取樣區間
-t = np.arange(0, 10, sI)   #設定取樣範圍、取樣區間
+freq = random.randint(0,4) #設定頻率
+t = np.arange(0, 10, 0.1)   #設定取樣範圍、取樣區間
 ft = 10*np.cos(2*np.pi*freq*t) #10*cos(2*pi*f*t)
 n = len(ft)
 t1 = t[range(int(n/2))]
